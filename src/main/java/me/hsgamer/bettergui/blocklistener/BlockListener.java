dcc520a3e27f502c7394e74041470dc804facc08
HSGamer <huynhqtienvtag@gmail.com>
2020-02-23 19:35:21 +0700

    add listener

--- src/main/java/me/hsgamer/bettergui/blocklistener/BlockListener.java
+++ src/main/java/me/hsgamer/bettergui/blocklistener/BlockListener.java
@@ -1,7 +1,35 @@
 package me.hsgamer.bettergui.blocklistener;
 
+import static me.hsgamer.bettergui.blocklistener.Main.getStorage;
+
+import me.hsgamer.bettergui.BetterGUI;
+import me.hsgamer.bettergui.config.impl.MessageConfig.DefaultMessage;
+import me.hsgamer.bettergui.util.CommonUtils;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class BlockListener implements Listener {
-
+  @EventHandler
+  public void onInteract(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    if (event.hasBlock()) {
+      Block block = event.getClickedBlock();
+      if (block != null) {
+        Location loc = block.getLocation();
+        if (getStorage().contains(loc)) {
+          String menu = getStorage().getMenuFromLocation(loc);
+          if (BetterGUI.getInstance().getMenuManager().contains(menu)) {
+            BetterGUI.getInstance().getMenuManager().openMenu(menu, player);
+          } else {
+            CommonUtils.sendMessage(player, BetterGUI.getInstance().getMessageConfig().get(
+                DefaultMessage.MENU_NOT_FOUND));
+          }
+        }
+      }
+    }
+  }
 }
